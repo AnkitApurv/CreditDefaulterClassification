d0abd8c6a286a98c1a675df79f266577f655a750
Ankit Apurv <k.ankit.capricon@gmail.com>
2020-07-28 14:33:22 +0530

    solved environment issues
    1.  stopped tracting .ipynb_checkpoints and 02BenchmarkDecisionTree.dot
    
    2. Created RegisterEnvAsKernel.py for easy kernel creation
    
    3. Modified environment.yml to include xlrd and change tensorflow to tensorflow-gpu
    
    4. Changed kernel  to project specific one for all notebooks

---
+++ registerEnvAsKernel.py
@@ -0,0 +1,5 @@
+#https://medium.com/@nrk25693/how-to-add-your-conda-environment-to-your-jupyter-notebook-in-just-4-steps-abeab8b8d084
+
+import os
+
+os.system('python -m ipykernel install --user --name=CreditDefaulterClassification')
